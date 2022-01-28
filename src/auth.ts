43b4b3a2be0932624dd38e8072a1f6933e682733
Jan Kaul <jankaul@mailbox.org>
2022-01-28 15:41:30 +0100

    setup session secret

--- src/auth.ts
+++ src/auth.ts
@@ -4,4 +4,8 @@ const { publicKey, privateKey } = await jose.generateKeyPair('RSA-OAEP-256')
 
 export const exportPublicKey = await jose.exportSPKI(publicKey)
 
+const cookieSecret = await jose.generateSecret("HS256")
+
+export const exportCookieSecret = JSON.stringify(await jose.exportJWK(cookieSecret as jose.KeyLike))
+
 export { privateKey }
--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,17 @@
 import { default as express } from "express"
 import { default as cors } from "cors"
 import { default as bodyParser } from "body-parser"
+import { default as session } from "express-session"
 import * as jose from "jose"
 import { Optional, Some, None } from "optional-typescript"
 import { match } from "ts-pattern"
-
-import { exportPublicKey, privateKey } from "./auth"
 import { MongoClient } from "mongodb";
+
+import { exportPublicKey, privateKey, exportCookieSecret } from "./auth"
+
 // import { runMongoDb } from "./database"
 
 let client: Optional<MongoClient> = None()
-
-let sessions = new Map()
 
 try {
     const app = express();
@@ -19,7 +19,12 @@ try {
 
     app.use(express.json())
     app.use(bodyParser.text())
-    app.use(cors());
+    app.use(cors({ origin: true, credentials: true }));
+    app.use(session({
+        secret: exportCookieSecret,
+        resave: false,
+        saveUninitialized: true,
+    }))
 
     // runMongoDb(app).catch(console.dir);
 
@@ -37,7 +42,7 @@ try {
 
         const sessionSecret = await jose.generateSecret('A256GCM') as jose.KeyLike
 
-
+        req["session"].secret = await jose.exportJWK(sessionSecret)
 
         const answer = await new jose.EncryptJWT({
             secret: encodeURIComponent(JSON.stringify(await jose.exportJWK(sessionSecret)))
@@ -45,6 +50,24 @@ try {
             .setProtectedHeader({ alg: 'RSA-OAEP-256', enc: 'A256GCM' })
             .setIssuedAt()
             .encrypt(await jose.importSPKI(decodeURIComponent(clientPublicKey as string), 'RSA-OAEP-256'))
+
+        res.send(answer)
+    })
+
+    app.post('/close', async (req, res) => {
+        console.log(req["session"])
+
+        const sessionSecret = await jose.importJWK(req["session"].secret, 'A256GCM')
+
+        const { payload, protectedHeader } = await jose.jwtDecrypt(req.body, sessionSecret)
+        const { authorized } = payload
+
+        const answer = await new jose.EncryptJWT({
+            success: authorized
+        })
+            .setProtectedHeader({ alg: 'dir', enc: 'A256GCM' })
+            .setIssuedAt()
+            .encrypt(sessionSecret)
 
         res.send(answer)
     })
