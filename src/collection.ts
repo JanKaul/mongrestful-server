34123f95948ace0f537e5c04433a5981a555496f
Jan Kaul <jankaul@mailbox.org>
2022-01-31 17:25:45 +0100

    insertMany

--- src/collection.ts
+++ src/collection.ts
@@ -69,4 +69,36 @@ export function collectionRoutes(dbName: string, dbOptions: DbOptions, collectio
 
         res.send(answer)
     })
+
+    app.post("/" + dbName + "/" + collectionName + "/insertmany", async (req, res) => {
+
+        const sessionSecret = await jose.importJWK(req["session"].secret, 'A256GCM')
+
+        const { payload, protectedHeader } = await jose.jwtDecrypt(req.body, sessionSecret)
+        const { docs, options } = payload
+
+        const result = await match(client)
+            .with({ tag: "some" }, async (x) => {
+                let result;
+                try {
+                    const db = (await x.value.connect()).db(dbName)
+                    const collection = db.collection(collectionName as string, collectionOptions as CollectionOptions)
+                    result = ok(await collection.insertMany(docs as unknown[], options))
+                } catch (error) {
+                    result = err(error.toString())
+                }
+                return result
+            })
+            .with({ tag: "none" }, async _ => err("Error: There is no MongoDB client running."))
+            .exhaustive()
+
+        const answer = await new jose.EncryptJWT({
+            result: result
+        })
+            .setProtectedHeader({ alg: 'dir', enc: 'A256GCM' })
+            .setIssuedAt()
+            .encrypt(sessionSecret)
+
+        res.send(answer)
+    })
 }
